c4d922274b9f12b114eeb306bfee88239ad527d9
akioiwaki <akioiwaki@gmail.com>
2017-01-04 06:12:19 +0000

    before_fileupload

--- app/controllers/submit_controller.rb
+++ app/controllers/submit_controller.rb
@@ -2,7 +2,6 @@ class SubmitController < ApplicationController
     layout 'top'
     
 def index
-    # 入力画面を表示
     @submit = Submit.new
     render :action => 'index'
 end
@@ -11,7 +10,7 @@ def confirm
     check_params = params.permit(:num, {:check => []}, :hoge)
     params.permit!
     # 入力値のチェック
-    @submit = submit.new(params[:submit])
+    @submit = Submit.new(params[:submit])
 
 
 if @submit.valid?
@@ -27,7 +26,7 @@ end
 def thanks
     params.permit!
     # メール送信
-    @submit = submit.new(params[:submit])
+    @submit = Submit.new(params[:submit])
     SubmitMailer.received_email(@submit).deliver
     # 完了画面を表示
     render :action => 'thanks'
@@ -36,7 +35,7 @@ end
 # private
     print
       def submit_params
-        params.require(:submit).permit(:sirname_kana, :firstname_kana, :postcode1, :postcode2, :prefecture, :city, :town, :town, :tel, :check, :subject, :email, :message)
+        params.require(:submit).permit(:sirname_kana, :firstname_kana, :postcode1, :postcode2, :prefecture, :city, :town, :town, :tel, :check, :email, :message)
     
       
       end
